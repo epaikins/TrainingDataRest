66bef9a3169d472a18dbf6bfb75b28c64cea51ee
epaikins <eugene.pokoo-aikins@amalitech.org>
2021-05-20 19:28:27 +0000

    Added Spring data rest and removed the service and controller components

--- src/main/java/org/amalitech/traineesdatarest/entity/Trainee.java
+++ src/main/java/org/amalitech/traineesdatarest/entity/Trainee.java
@@ -1,21 +1,31 @@
 package org.amalitech.traineesdatarest.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 
 @Entity
-@Data
+@Getter
+@Setter
 public class Trainee {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int traineeID;
+	
+	@Column(name="firstName")
 	private String firstName;
+	
+	@Column(name="lastName")
 	private String lastName;
+	
+	@Column(name="email")
 	private String email;
 }
--- src/main/java/org/amalitech/traineesdatarest/repository/TraineeRepository.java
+++ src/main/java/org/amalitech/traineesdatarest/repository/TraineeRepository.java
@@ -1,10 +1,12 @@
 package org.amalitech.traineesdatarest.repository;
 
-import org.springframework.stereotype.Repository;
+//import org.springframework.stereotype.Repository;
 import org.amalitech.traineesdatarest.entity.Trainee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
-@Repository
+@RepositoryRestResource(collectionResourceRel="trainee", path="trainee")
 public interface TraineeRepository extends JpaRepository<Trainee, Integer>{
 
 }
